a9e36a95f12b8b2bb7a08d600c169f89872a6774
DELL <omnianabil1920@gmail.com>
2024-07-16 12:15:45 +0300

    created data classes

---
+++ app/src/main/java/com/example/movieappandroid/model/Result.java
@@ -0,0 +1,58 @@
+package com.example.movieappandroid.model;
+
+import android.graphics.Movie;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class Result {
+    @SerializedName("page")
+    @Expose
+    private Integer pages;
+    @SerializedName("total_pages")
+    @Expose
+    private Integer totalPages;
+    @SerializedName("total_results")
+    @Expose
+    private Integer totalResults;
+    @SerializedName("results")
+    @Expose
+    private List<Movie> results = null;
+
+    public Integer getPages() {
+        return pages;
+    }
+
+    public void setPages(Integer pages) {
+        this.pages = pages;
+    }
+
+    public Integer getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(Integer totalPages) {
+        this.totalPages = totalPages;
+    }
+
+    public Integer getTotalResults() {
+        return totalResults;
+    }
+
+    public void setTotalResults(Integer totalResults) {
+        this.totalResults = totalResults;
+    }
+
+    public List<Movie> getResults() {
+        return results;
+    }
+
+    public void setResults(List<Movie> results) {
+        this.results = results;
+    }
+
+    public Result() {
+    }
+}
